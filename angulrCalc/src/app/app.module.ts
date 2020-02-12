45c1252b1285274f5328a6be2c58763c3dbc0e88
abou <zoobi.92@gmail.com>
2020-02-13 02:11:11 +0530

    angular project calc

---
+++ angulrCalc/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalcComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
